0d88c213443d11c53ee99558c36f9d3c90a5faee
Ziper0121 <gofrktl13@naver.com>
2022-12-30 17:08:53 +0900

    메타 프로그래밍을 이용하여 최대공약수를 구하는 템플릿 함수를 만들었습니.

--- Test_and_Quiz/Test_and_Quiz/TestProject.cpp
+++ Test_and_Quiz/Test_and_Quiz/TestProject.cpp
@@ -10,3 +10,20 @@ void RegexTest()
 
 	std::cout << string << " : " << std::boolalpha << std::regex_match(string, regex) << std::endl;
 }
+
+template<int A, int B>
+struct GCD
+{
+	static const int result = GCD<B,A % B>::result;
+};
+
+template<int A>
+struct GCD<A,0>
+{
+	static const int result = A;
+};
+
+void Metaprograming()
+{
+	int N = GCD<20, 4>::result;
+}
--- Test_and_Quiz/Test_and_Quiz/TestProject.h
+++ Test_and_Quiz/Test_and_Quiz/TestProject.h
@@ -1,3 +1,5 @@
 #pragma once
 
 void RegexTest();
+
+void Metaprograming();
